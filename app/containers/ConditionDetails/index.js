da2bbb240fb62b428cfcd90cb20404e036e0dbec
Kevin Simpson <relasine@gmail.com>
2019-06-20 20:18:42 -0600

    Add layout and styling for Condition Details

--- app/containers/ConditionDetails/index.js
+++ app/containers/ConditionDetails/index.js
@@ -10,21 +10,47 @@ import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
+import { NavLink } from 'react-router-dom';
+import './styles.css';
 
 import injectReducer from 'utils/injectReducer';
+import { mockConditionDetails } from '../../utils/mockData';
 import makeSelectConditionDetails from './selectors';
 import reducer from './reducer';
+import mountains from '../../images/mountain-range.svg';
 
 /* eslint-disable react/prefer-stateless-function */
 export class ConditionDetails extends React.Component {
   render() {
+    const { trailName, description, image, location } = mockConditionDetails;
+
     return (
-      <div>
+      <div className="condition-details">
         <Helmet>
           <title>ConditionDetails</title>
           <meta name="description" content="Description of ConditionDetails" />
         </Helmet>
-        <h1>Condition Details</h1>
+        <h1 className="condition-details__header">Condition Details</h1>
+        <div
+          style={{
+            backgroundImage: `url(${mountains})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+          className="condition-details__icon"
+        />
+        <h4 className="condition-details__sub-header">{trailName}</h4>
+        <img src={image.url} alt={image.description} className="condition-details__trail-image" />
+        <p className="condition-details__description">
+          <strong>Trail description:</strong> {description}
+        </p>
+        <p className="condition-details__location">
+          <strong>Location: </strong>
+          {location}
+        </p>
+        <NavLink className="condition-details__link" to="/conditions">
+          Return to List
+        </NavLink>
       </div>
     );
   }
---
+++ app/utils/mockData.js
@@ -0,0 +1,11 @@
+export const mockConditionDetails = {
+  trailName: 'Eastwood Ravine',
+  image: {
+    url:
+      'http://radnorlake.org/wp-content/gallery/trails/South-Cove-Trail-Pix-December-23rd-2014-by-Steve-Ward-24.jpg',
+    descriptiion: 'placeholder',
+  },
+  description:
+    'Laborum adipisicing aliqua tempor id sit amet reprehenderit do nisi elit esse aliqua ipsum magna. Duis quis pariatur consectetur ex excepteur esse dolore laborum officia pariatur. Est amet ex voluptate non deserunt magna magna. Labore eiusmod ea consectetur est aliqua elit do tempor voluptate laboris nostrud proident enim. Ea ad consequat velit quis do ipsum eu veniam in qui minim.',
+  location: 'Lat: 60, Long: -39',
+};
