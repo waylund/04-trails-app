99f068f6074cb83939ba6bc700be63afd5623370
dmta9796 <dmta9796@colorado.edu>
2019-06-20 20:52:37 -0600

    start of confirmation page

---
+++ app/components/ConfirmationComponent/tests/index.test.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { mount } from 'enzyme';
+// import { enzymeFind } from 'styled-components/test-utils';
+
+// import ConfirmationComponent from '../index';
+
+describe('<ConfirmationComponent />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
